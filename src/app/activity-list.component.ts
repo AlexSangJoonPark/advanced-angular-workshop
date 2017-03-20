f68e34cf913dff5c76fd9680fa260bad3b147043
Alex Park <alex.park.ict@gmail.com>
2017-03-21 09:54:54 +1100

    Updated for activity list

---
+++ src/app/activity-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
+
+@Component({
+  selector: 'app-activity-list',
+  template: `
+<div *ngFor="let activity of activities | async" class="activity">
+  <h2>{{activity.title}}</h2>
+  <p>{{activity.description}}</p>
+</div>
+  `,
+  styles: []
+})
+export class ActivityListComponent {
+
+  activities: Observable<any>;
+
+  constructor(http: Http) {
+    let activities = http.get('https://melbourne-things-to-do.firebaseio.com/activities.json')
+      .map(res => res.json());
+
+    activities.subscribe(data => {
+      localStorage['activityCache'] = JSON.stringify(data);
+    });
+
+    this.activities = activities.startWith(JSON.parse(localStorage['activityCache'] || null));
+  }
+
+}
